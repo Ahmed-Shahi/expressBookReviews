59b9d04b871b654fdc70d4230830d36f3b459f03
Ahmed-Shahi <ahmedshahi038@gmail.com>
2026-08-28 04:24:47 +0500

    task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,8 +23,15 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const keys = Object.keys(books);
+  const matchingBooks = [];
+  for (let key of keys) {
+    if (books[key].author === author) {
+      matchingBooks.push(books[key]);
+    }
+  }
+  res.send(JSON.stringify(matchingBooks, null, 4));
 });
 
 // Get all books based on title
